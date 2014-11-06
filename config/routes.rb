2893b1478e7665563832864ad7c14590c43e8cd3
Fakhitah Ridzuan <fakhitah.ridzuan@gmail.com>
2014-11-06 07:08:28 +0000

    Modify the layout using bootstrap and edit the root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Workspace::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/contact"
   
-  root 'static_pages#about'
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
