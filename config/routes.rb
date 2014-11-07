ad0fce09bb3647f4574f0d80fec5957c3f47ad00
Fakhitah Ridzuan <fakhitah.ridzuan@gmail.com>
2014-11-07 03:18:06 +0000

    Modify the validate message and add new signin page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Workspace::Application.routes.draw do
   resources :users
   
   get 'signup' => 'users#new'
+  get "users/create"
   
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20141106072316) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "pasword_digest"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
